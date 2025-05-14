de445411934fdbdbb674805c5957234beb55cef1
Jessica Zhang <jessica.zhang@oss.qualcomm.com>
2025-05-14 16:52:32 -0700

    drm/msm/dpu: Filter writeback modes using writeback maxlinewidth
    Since the max mixer width is not a strict hardware limit, use the actual
    hardware limit (the writeback maxlinewidth) to filter modes.
    
    Signed-off-by: Jessica Zhang <jessica.zhang@oss.qualcomm.com>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_writeback.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_writeback.c
@@ -14,14 +14,7 @@ static int dpu_wb_conn_get_modes(struct drm_connector *connector)
 	struct msm_drm_private *priv = dev->dev_private;
 	struct dpu_kms *dpu_kms = to_dpu_kms(priv->kms);
 
-	/*
-	 * We should ideally be limiting the modes only to the maxlinewidth but
-	 * on some chipsets this will allow even 4k modes to be added which will
-	 * fail the per SSPP bandwidth checks. So, till we have dual-SSPP support
-	 * and source split support added lets limit the modes based on max_mixer_width
-	 * as 4K modes can then be supported.
-	 */
-	return drm_add_modes_noedid(connector, dpu_kms->catalog->caps->max_mixer_width,
+	return drm_add_modes_noedid(connector, dpu_kms->catalog->wb->maxlinewidth,
 			dev->mode_config.max_height);
 }
 
