2aeaf0f01f8d11a1baa06d1c741c2500529b797c
Xilin Wu <sophon@radxa.com>
2025-07-22 18:01:54 +0800

    soc: qcom: geni-se: Add MODULE_FIRMWARE macro
    Signed-off-by: Xilin Wu <sophon@radxa.com>

--- drivers/soc/qcom/qcom-geni-se.c
+++ drivers/soc/qcom/qcom-geni-se.c
@@ -1449,3 +1449,4 @@ module_platform_driver(geni_se_driver);
 
 MODULE_DESCRIPTION("GENI Serial Engine Driver");
 MODULE_LICENSE("GPL v2");
+MODULE_FIRMWARE("qcom/qcm6490/qupv3fw.elf");
--- drivers/tty/serial/qcom_geni_serial.c
+++ drivers/tty/serial/qcom_geni_serial.c
@@ -1948,3 +1948,4 @@ module_exit(qcom_geni_serial_exit);
 
 MODULE_DESCRIPTION("Serial driver for GENI based QUP cores");
 MODULE_LICENSE("GPL v2");
+MODULE_FIRMWARE("qcom/qcm6490/qupv3fw.elf");
