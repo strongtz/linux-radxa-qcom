2fb11a5328914e87d4a3513ceeff84602365531d
Stephan Gerhold <stephan.gerhold@linaro.org>
2025-08-06 13:27:46 +0200

    media: iris: Port firmware loading without TZ/PAS from venus
    On Qualcomm compute platforms such as X1E, there is community-maintained
    support for running without the Qualcomm hypervisor (QHEE/Gunyah) in EL2.
    This is enabled by applying the -el2.dtso (here: x1-el2.dtso).
    
    As shown by Mukesh, the reset and IOMMU management for remoteprocs like
    Iris is implemented in the hypervisor for older targets such as X1E [1],
    so this functionality is missing when running in EL2.
    
    To avoid introducing kernel errors for this configuration when adding Iris
    support for X1E, we need to add support for starting Iris without the
    firmware PAS service in the TZ or hypervisor firmware. All the necessary
    code for this already exists in venus/firmware.c, it was just not ported
    over to iris. The "video-firmware" node is even documented in the
    dt-bindings for iris (through qcom,venus-common.yaml), but not currently
    used by the driver.
    
    Port the necessary code for this from venus/firmware.c, but remove the old
    A9SS sequence and do some cleanup to fit better into the new iris driver.
    For now the "non_tz" boot sequence is used when there is a "video-firmware"
    subnode defined in the device tree with the IOMMU for the Iris firmware.
    There is ongoing work for newer platforms to use TZ/PAS for resets and
    perform just the IOMMU management in Linux. The IOMMU code in this commit
    can be reused for that by setting core->use_tz set to true.
    
    [1]: https://resources.linaro.org/en/resource/sF8jXifdb9V1mUefdbfafa
    
    Signed-off-by: Stephan Gerhold <stephan.gerhold@linaro.org>
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_probe.c
+++ drivers/media/platform/qcom/iris/iris_probe.c
@@ -13,6 +13,7 @@
 
 #include "iris_core.h"
 #include "iris_ctrls.h"
+#include "iris_firmware.h"
 #include "iris_vidc.h"
 
 static int iris_init_icc(struct iris_core *core)
@@ -199,6 +200,8 @@ static void iris_remove(struct platform_device *pdev)
 
 	v4l2_device_unregister(&core->v4l2_dev);
 
+	iris_fw_deinit(core);
+
 	mutex_destroy(&core->lock);
 }
 
@@ -256,6 +259,10 @@ static int iris_probe(struct platform_device *pdev)
 	core->iris_platform_data->init_hfi_response_ops(core);
 
 	ret = iris_init_resources(core);
+	if (ret)
+		return ret;
+
+	ret = iris_fw_init(core);
 	if (ret)
 		return ret;
 
