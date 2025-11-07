53cb09822381d381d5844a02889da8b888f9d03d
Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
2025-11-07 10:13:18 +0530

    PCI: qcom: Check for the presence of a device instead of Link up during suspend
    The suspend handler checks for the PCIe Link up to decide when to turn off
    the controller resources. But this check is racy as the PCIe Link can go
    down just after this check.
    
    So use the newly introduced API, pci_root_ports_have_device() that checks
    for the presence of a device under any of the Root Ports to replace the
    Link up check.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>

--- drivers/pci/controller/dwc/pcie-qcom.c
+++ drivers/pci/controller/dwc/pcie-qcom.c
@@ -1925,6 +1925,7 @@ err_pm_runtime_put:
 static int qcom_pcie_suspend_noirq(struct device *dev)
 {
 	struct qcom_pcie *pcie;
+	struct dw_pcie_rp *pp;
 	int ret = 0;
 
 	pcie = dev_get_drvdata(dev);
@@ -1960,8 +1961,9 @@ static int qcom_pcie_suspend_noirq(struct device *dev)
 	 * powerdown state. This will affect the lifetime of the storage devices
 	 * like NVMe.
 	 */
-	if (!dw_pcie_link_up(pcie->pci)) {
-		qcom_pcie_host_deinit(&pcie->pci->pp);
+	pp = &pcie->pci->pp;
+	if (!pci_root_ports_have_device(pp->bridge->bus)) {
+		qcom_pcie_host_deinit(pp);
 		pcie->suspended = true;
 	}
 
