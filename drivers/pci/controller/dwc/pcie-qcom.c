b131ffe9da659bdd7e8c667dc2a6b0f694b32370
Qiang Yu <qiang.yu@oss.qualcomm.com>
2025-11-09 22:59:44 -0800

    PCI: qcom: Remove DPC Extended Capability
    Some platforms (e.g., X1E80100) expose Downstream Port Containment (DPC)
    Extended Capability registers in the PCIe Root Port config space, but do
    not fully support it. To prevent undefined behavior and ensure DPC cap is
    not visible to PCI framework and users, remove DPC Extended Capability
    unconditionally, since there is no qcom platform support DPC till now.
    
    Co-developed-by: Wenbin Yao <wenbin.yao@oss.qualcomm.com>
    Signed-off-by: Wenbin Yao <wenbin.yao@oss.qualcomm.com>
    Signed-off-by: Qiang Yu <qiang.yu@oss.qualcomm.com>

--- drivers/pci/controller/dwc/pcie-qcom.c
+++ drivers/pci/controller/dwc/pcie-qcom.c
@@ -1298,6 +1298,7 @@ static int qcom_pcie_host_init(struct dw_pcie_rp *pp)
 
 	qcom_pcie_clear_aspm_l0s(pcie->pci);
 	dw_pcie_remove_capability(pcie->pci, PCI_CAP_ID_MSIX);
+	dw_pcie_remove_ext_capability(pcie->pci, PCI_EXT_CAP_ID_DPC);
 
 	qcom_ep_reset_deassert(pcie);
 
