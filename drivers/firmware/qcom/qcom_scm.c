8ce5fb13a3dab15e66de232aed70c34dec8ad226
Xilin Wu <sophon@radxa.com>
2026-01-13 21:31:48 +0800

    firmware: qcom: scm: Allow QSEECOM for Radxa Dragon Q8B
    add "radxa,dragon-q8b" as compatible device for QSEECOM
    
    This is required to get access to efivars and uefi boot loader support.
    
    Signed-off-by: Xilin Wu <sophon@radxa.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -2339,6 +2339,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "qcom,x1e80100-qcp" },
 	{ .compatible = "qcom,x1p42100-crd" },
 	{ .compatible = "radxa,dragon-q6a" },
+	{ .compatible = "radxa,dragon-q8b" },
 	{ }
 };
 
